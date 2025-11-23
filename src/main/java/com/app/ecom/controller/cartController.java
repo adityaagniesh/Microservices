79a85468eb0fcea922948c343649d6c2e79c906a
aditya-agniesh <aditya.agniesh@gmail.com>
2025-11-23 19:26:49 +0530

    adding item to cart

---
+++ src/main/java/com/app/ecom/controller/cartController.java
@@ -0,0 +1,26 @@
+package com.app.ecom.controller;
+
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cart")
+public class cartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<String> addToCart (
+            @RequestHeader("X-User-ID") String userId,
+            @RequestBody CartItemRequest request) {
+        if(!cartService.addToCart(userId, request)){
+            return ResponseEntity.badRequest().body("Product Out of Stock or User not Found or Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
